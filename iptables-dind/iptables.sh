0124e09aa47a805cd01020bc44324dc563ea1157
Aarni Halinen <aarni.halinen@gmail.com>
2023-03-08 12:25:54 +0200

    Move iptables to shell script

--- iptables-dind/iptables.sh
+++ iptables-dind/iptables.sh
@@ -1,9 +1,19 @@
-SIDECAR_UID=2000
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+set -x
+# SIDECAR_UID=2000
+SIDECAR_CONFIG=$(docker ps -q | xargs docker inspect --format '{{.State.Pid}},{{.Config.User}},{{.Name}}' | grep sidecar)
+
+SIDECAR_PID=$(echo $SIDECAR_CONFIG | cut -f1 -d,)
+SIDECAR_UID=$(echo $SIDECAR_CONFIG | cut -f2 -d,)
+
+# nsenter -t $SIDECAR_PID -n
+
+nsenter -t $SIDECAR_PID -n iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # Build chain based on uid
-iptables --new-chain sidecar_user
-iptables -A OUTPUT -m owner --uid-owner $SIDECAR_UID -j sidecar_user
+nsenter -t $SIDECAR_PID -n iptables --new-chain sidecar_user
+nsenter -t $SIDECAR_PID -n iptables -A OUTPUT -m owner --uid-owner $SIDECAR_UID -j sidecar_user
 
 # Chain rules
-iptables -A sidecar_user -j REJECT
+nsenter -t $SIDECAR_PID -n iptables -A sidecar_user -j REJECT
+
+while true; do sleep 1; done
