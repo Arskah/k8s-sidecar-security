b3f80615a56597cc362b2454c388ae7abb0c3b09
Aarni Halinen <aarni.halinen@gmail.com>
2023-03-22 17:21:37 +0200

    Disable ebpf image loading

--- cilium-setup.sh
+++ cilium-setup.sh
@@ -9,16 +9,18 @@ set -x
 # Requires Hubble CLI
 # See cilium-install-sh
 
-minikube start --nodes 2 --network-plugin=cni --cni=false
+minikube start --nodes 2 --cni=false
 cilium install
 cilium hubble enable --ui
+
+# minikube start --nodes 2 --cni=cilium --container-runtime=cri-o
 
 kubectl get node -l '!node-role.kubernetes.io/control-plane' -o custom-columns=NAME:.metadata.name | tail -n +2 | xargs -I{} kubectl label nodes {} kubernetes-worker= node-role.kubernetes.io/worker=
 kubectl label namespace kube-system name=kube-system
 kubectl label nodes --all kubernetes-host=
 
 minikube image load malicious-sidecar
-minikube image load go-ebpf
+# minikube image load go-ebpf
 
 cilium status
 # cilium connectivity test
