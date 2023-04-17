430341a5bc62ec76e4779d3b4edd8fb334c03df2
Aarni Halinen <aarni.halinen@gmail.com>
2023-04-17 15:33:24 +0300

    Add calico-setup

---
+++ calico-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -x
+
+minikube start --nodes 2 --cni=calico
+# kubectl create clusterrolebinding cluster-system-anonymous --clusterrole=cluster-admin --user=system:anonymous
+
+kubectl get node -l '!node-role.kubernetes.io/control-plane' -o custom-columns=NAME:.metadata.name | tail -n +2 | xargs -I{} kubectl label nodes {} kubernetes-worker= node-role.kubernetes.io/worker=
+kubectl label namespace kube-system name=kube-system
+kubectl label nodes --all kubernetes-host=
+
+minikube image load malicious-sidecar
+
+# calicoctl --allow-version-mismatch patch kubecontrollersconfiguration default --patch='{"spec": {"controllers": {"node": {"hostEndpoint": {"autoCreate": "Enabled"}}}}}'
+# calicoctl --allow-version-mismatch apply -f calico/global-network-policy.yml
+
+kubectl apply -f cluster/
