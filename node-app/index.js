a5b3f01f810614c8991a72edfeb3e4b903aebf3c
Aarni Halinen <aarni.halinen@gmail.com>
2023-07-19 00:11:06 +0300

    Fix node-app

--- node-app/index.js
+++ node-app/index.js
@@ -1,28 +1,45 @@
 const Koa = require('koa');
-const app = new Koa();
-
+const logger = require('koa-logger')
+const Router = require('koa-router');
 const StatsD = require('node-statsd');
+
+const app = new Koa();
+const router = new Router();
 const client = new StatsD();
 
-const logMessage = async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-  client.timing('response_time', rt);
-  client.increment('response_counter');
-}
-
-const setResponseTimeCtx =async (ctx, next) => {
+const setResponseTimeCtx = async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
   ctx.set('X-Response-Time', `${ms}ms`);
 }
 
-app.use(logMessage);
-app.use(setResponseTimeCtx);
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const sendStats = (async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`rt: ${ctx.method} ${ctx.url} - ${rt}`);
+  client.timing('response_time', rt);
+  client.increment('response_counter');
 });
 
-app.listen(8888);
+const errorHandler = (err, ctx) => {
+  console.error("Server error: ", err, ctx)
+}
+
+const requestHandler = async ctx => {
+  ctx.body = 'Hello World';
+}
+
+router.get("/", requestHandler)
+
+app.use(logger())
+app.use(sendStats);
+app.use(setResponseTimeCtx);
+app.on("error", errorHandler)
+app.use(router.routes())
+app.use(router.allowedMethods());
+
+
+app.listen(8888, () => {
+  console.log("Server start")
+});
