1f1009df539aef7e701854838659508b19805e63
Aarni Halinen <aarni.halinen@gmail.com>
2023-07-18 23:22:21 +0300

    Fix protocol

--- solutions/iptables-injector/localhost-redirect.sh
+++ solutions/iptables-injector/localhost-redirect.sh
@@ -5,7 +5,7 @@ set -x
 MAIN_PID=$(crictl -r unix:///var/run/containerd/containerd.sock ps --name node-app -q | xargs crictl -r unix:///var/run/containerd/containerd.sock inspect | jq '.info.pid')
 
 nsenter -t $MAIN_PID -n sysctl -w net.ipv4.conf.all.route_localnet=1
-nsenter -t $MAIN_PID -n iptables -t nat -A OUTPUT -p TCP --dport 8125 -j DNAT --to-destination 192.168.1.202:8125
+nsenter -t $MAIN_PID -n iptables -t nat -A OUTPUT -p UDP -o lo --dport 8125 -j DNAT --to-destination 192.168.1.202:8125
 nsenter -t $MAIN_PID -n iptables -t nat -A POSTROUTING -j MASQUERADE
 
 set +x
