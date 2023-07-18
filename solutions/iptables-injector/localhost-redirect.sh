4ea249b354f73b2cd156d7f0f7e3effa4631ebfe
Aarni Halinen <aarni.halinen@gmail.com>
2023-07-18 22:52:36 +0300

    Restructuring and small fixes

---
+++ solutions/iptables-injector/localhost-redirect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+MAIN_PID=$(crictl -r unix:///var/run/containerd/containerd.sock ps --name node-app -q | xargs crictl -r unix:///var/run/containerd/containerd.sock inspect | jq '.info.pid')
+
+nsenter -t $MAIN_PID -n sysctl -w net.ipv4.conf.all.route_localnet=1
+nsenter -t $MAIN_PID -n iptables -t nat -A OUTPUT -p TCP --dport 8125 -j DNAT --to-destination 192.168.1.202:8125
+nsenter -t $MAIN_PID -n iptables -t nat -A POSTROUTING -j MASQUERADE
+
+set +x
+
+while true; do sleep 1; done
--- solutions/multi-ns/start.sh
+++ solutions/multi-ns/start.sh
@@ -3,9 +3,13 @@
 set -x
 
 minikube start --nodes 2 --cni=calico --container-runtime=containerd
+
 minikube image load node-app
+minikube image load iptables-injector
 
 kubectl apply -R -f multus/
 kubectl apply -f namespace.yml
 kubectl apply -f local-network.yml
 kubectl apply -R -f app/
+
+kubectl apply -f redirect-injector.yml
