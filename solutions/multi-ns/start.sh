3e958deae7350ff5d97120fb05e48f3202515027
Aarni Halinen <aarni.halinen@gmail.com>
2023-07-18 22:24:13 +0300

    Add multi-ns solution

---
+++ solutions/multi-ns/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -x
+
+minikube start --nodes 2 --cni=calico --container-runtime=containerd
+minikube image load node-app
+
+kubectl apply -R -f multus/
+kubectl apply -f namespace.yml
+kubectl apply -f local-network.yml
+kubectl apply -R -f app/
