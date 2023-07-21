321236e6d911803133b192751021773b6641c8f5
Aarni Halinen <aarni.halinen@gmail.com>
2023-07-21 20:44:34 +0300

    Update iptables rules

--- solutions/iptables-injector/iptables.sh
+++ solutions/iptables-injector/iptables.sh
@@ -9,18 +9,18 @@ set -x
 # Ingress rules
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport $APP_PORT -j ACCEPT
-iptables -A INPUT -p tcp --dport $SIDECAR_PORT -j ACCEPT
+iptables -A INPUT -p udp --dport $SIDECAR_PORT -j ACCEPT
 iptables -A INPUT -j DROP
 
 # Egress rules
 iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 ## Main app rules
-iptables -A OUTPUT -d 127.0.0.1 -m owner --uid-owner $APP_UID -p tcp --dport $APP_PORT -j ACCEPT
-iptables -A OUTPUT -d 127.0.0.1 -m owner --uid-owner $APP_UID -p tcp --dport $SIDECAR_PORT -j ACCEPT
+iptables -A OUTPUT -o lo -m owner --uid-owner $APP_UID -p tcp --dport $APP_PORT -j ACCEPT
+iptables -A OUTPUT -o lo -m owner --uid-owner $APP_UID -p udp --dport $SIDECAR_PORT -j ACCEPT
 
 ## Sidecar rules
-iptables -A OUTPUT -d 127.0.0.1 -m owner --uid-owner $SIDECAR_UID -p tcp --dport $SIDECAR_PORT -j ACCEPT
+iptables -A OUTPUT -o lo -m owner --uid-owner $SIDECAR_UID -p udp --dport $SIDECAR_PORT -j ACCEPT
 # iptables -A OUTPUT -m owner --uid-owner $SIDECAR_UID -j REJECT
 
 iptables -A OUTPUT -j REJECT
