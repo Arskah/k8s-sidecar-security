e67a900b1e93132b986f670d7c7cd516d28f5015
Aarni Halinen <aarni.halinen@gmail.com>
2023-12-30 19:44:53 +0200

    add docker cmds

--- solutions/iptables-injector/iptables-file.sh
+++ solutions/iptables-injector/iptables-file.sh
@@ -8,6 +8,9 @@ set -x
 # SIDECAR_CONFIG=$(crictl ps --name statsd -q| xargs crictl inspect)
 SIDECAR_PID=$(crictl -r unix:///var/run/containerd/containerd.sock ps --name statsd -q | xargs crictl -r unix:///var/run/containerd/containerd.sock inspect | jq '.info.pid')
 
+# SIDECAR_CONFIG=$(docker ps -q | xargs docker inspect --format '{{.State.Pid}},{{.Config.User}},{{.Name}}' | grep sidecar)
+# SIDECAR_PID=$(echo $SIDECAR_CONFIG | cut -f1 -d,)
+
 nsenter -t $SIDECAR_PID -n iptables-restore < iptables.rules
 
 set +x
